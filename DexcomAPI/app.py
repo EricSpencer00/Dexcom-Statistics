9bf2a55ef89980fc5ab55ff570bceee3577fea8a
EricSpencer00 <119877507+EricSpencer00@users.noreply.github.com>
2024-05-12 12:43:03 -0500

    return data in api call

--- DexcomAPI/app.py
+++ DexcomAPI/app.py
@@ -2,16 +2,21 @@
 '''
 Initialize a webapp using Flask
 '''
-from flask import Flask
 from main import get_dexcom_connection
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/api/data', methods=['GET'])
+def get_number():
     dexcom = get_dexcom_connection()
     result = dexcom.get_current_glucose_reading()
-    return result
+    return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
