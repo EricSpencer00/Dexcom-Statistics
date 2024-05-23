40aaaf515fafed06c33f38eaeb3dfc71bc5160c0
EricSpencer00 <119877507+EricSpencer00@users.noreply.github.com>
2024-05-23 15:59:26 -0500

    latest noti, db error handling

--- DexcomAPI/database.py
+++ DexcomAPI/database.py
@@ -5,38 +5,63 @@ import datetime
 import mysql.connector
 import pydexcom
 import os
+from mysql.connector import Error
 from defs import get_dexcom_connection, get_database_connection
 
 def get_latest_timestamp(cursor, db_name):
-    query = f"SELECT MAX(timestamp) FROM {db_name}"
-    cursor.execute(query)
-    result = cursor.fetchone()
-    return result[0] if result[0] is not None else None
+    try:
+        query = f"SELECT MAX(timestamp) FROM {db_name}"
+        cursor.execute(query)
+        result = cursor.fetchone()
+        return result[0] if result[0] is not None else None
+    except Error as e:
+        print(f"Error: ", e)
+        return None
+
+def get_latest_notifcation(cursor, db_name):
+    try:
+        query = f"SELECT notification FROM {db_name} ORDER BY timestamp DESC LIMIT 1"
+        cursor.execute(query)
+        latest_notification = cursor.fetchone()[0]
+
+        query = f"SELECT timestamp FROM {db_name} ORDER BY timestamp DESC LIMIT 6"
+        cursor.execute(query)
+        latest_timestamps = [row[0] for row in cursor.fetchall()]
+
+        return latest_notification, latest_timestamps
+    except Error as e:
+        print(f"Error: ", e)
+        return None, None
 
 def insert_glucose_readings(dexcom, db, db_name):
-    cursor = db.cursor()
+    try:
+        cursor = db.cursor()
 
-    latest_timestamp = get_latest_timestamp(cursor, db_name)
+        latest_timestamp = get_latest_timestamp(cursor, db_name)
 
-    glucose_readings = dexcom.get_glucose_readings(minutes=1440, max_count=288)
+        glucose_readings = dexcom.get_glucose_readings(minutes=1440, max_count=288)
 
-    insert_data = []
+        insert_data = []
 
-    now = datetime.datetime.now()
-    base_minute = now.minute - (now.minute % 5)
-    base_timestamp = now.replace(minute=base_minute, second=0, microsecond=0)
+        now = datetime.datetime.now()
+        base_minute = now.minute - (now.minute % 5)
+        base_timestamp = now.replace(minute=base_minute, second=0, microsecond=0)
 
-    for index in reversed(range(len(glucose_readings))):
-        reading = glucose_readings[index]
-        timestamp = base_timestamp - datetime.timedelta(minutes=index * 5)
+        for index in reversed(range(len(glucose_readings))):
+            reading = glucose_readings[index]
+            timestamp = base_timestamp - datetime.timedelta(minutes=index * 5)
 
-        # Ensure timestamps will not overlap and duplicate data in table
-        if latest_timestamp is None or timestamp > latest_timestamp:
-            mgdl_reading = reading.value
-            insert_data.append((timestamp, mgdl_reading))
+            # Ensure timestamps will not overlap and duplicate data in table
+            if latest_timestamp is None or timestamp > latest_timestamp:
+                mgdl_reading = reading.value
+                insert_data.append((timestamp, mgdl_reading))
 
-    if insert_data:
-        cursor.executemany(f"INSERT INTO {db_name} (timestamp, mgdl_reading) VALUES (%s, %s)", insert_data)
-        db.commit()
+        if insert_data:
+            cursor.executemany(f"INSERT INTO {db_name} (timestamp, mgdl_reading) VALUES (%s, %s)", insert_data)
+            db.commit()
 
-    cursor.close()
+        cursor.close()
+    except Error as e:
+        print(f"Error: ", e)
+        db.rollback()
+        cursor.close()
--- DexcomAPI/main.py
+++ DexcomAPI/main.py
@@ -1,18 +1,18 @@
-# MGDL numbers
+# main.py
+# MGDL Numbers
 
-# import os
-import time
+import os
 import smtplib
+import smtplib
+from datetime import datetime, timedelta
 from pydexcom import Dexcom
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import smtplib
 from defs import get_dexcom_connection, get_sender_email_credentials, get_receiver_email, get_database_connection
 from stat_functions import verbose_message_mgdl, verbose_message_mmol, concise_message_mdgl, concise_message_mmol
-# from database import insert_glucose_readings
+from database import insert_glucose_readings
 from stat_functions import get_current_value_mdgl, get_current_value_mmol
 
-# Initialize variables
 dexcom = get_dexcom_connection()
 email_username, email_password = get_sender_email_credentials()
 receiver_email = get_receiver_email()
@@ -31,6 +31,8 @@ last_notification = {}
 # print("\n")
 
 glucose_number = get_current_value_mdgl(dexcom)
+glucose_time = dexcom.get_current_glucose_reading().datetime
+print(glucose_time)
 
 if glucose_number <= 55 or glucose_number >= 180:
     if(time.time() - last_notification.get("time", 0) > 1800): # 30 minutes
@@ -50,3 +52,11 @@ if glucose_number <= 55 or glucose_number >= 180:
             print(f"Message sent successfully: {message}")
         except Exception as e:
             print(f"error: {e}")
+
+db = get_database_connection()
+db_name = os.getenv("sql_database")
+
+try:
+    insert_glucose_readings(dexcom, db, db_name)
+finally:
+    db.close()
